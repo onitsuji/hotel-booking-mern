4ae84ad114ed5e6958cec7bbaf14a1273be52b70
justin-0 <itsjustinoconnell@gmail.com>
2025-04-24 15:34:18 +0100

    root layout - styled header, nav

--- frontend/app/layout/root.tsx
+++ frontend/app/layout/root.tsx
@@ -1,10 +1,25 @@
+import { Link } from "react-router";
 import { Outlet } from "react-router";
 
 export default function RootLayout() {
   return (
     <div>
-      <header>
-        <nav>Navigation</nav>
+      <header className="bg-blue-500 text-white">
+        <nav className="flex justify-between container mx-auto py-4 items-center px-2 md:px-0">
+          <div>
+            <Link to="/" className="text-2xl font-bold">
+              Getaways
+            </Link>
+          </div>
+          <div>
+            <Link
+              to="/sign-in"
+              className="text-blue-500 bg-white px-3 py-1 rounded-md"
+            >
+              Sign in
+            </Link>
+          </div>
+        </nav>
       </header>
       <Outlet />
     </div>
